5182b21580208d461b83b5a376298ff3607aa985
Nadav Yayon <36442165+nadavyayon@users.noreply.github.com>
2024-01-26 22:39:29 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'OrganAxis'
+copyright = '2024, Nadav Yayon'
+author = 'Nadav Yayon'
 
 release = '0.1'
 version = '0.1.0'
